91077f04469c54a0162b0e672dfcfe95533c2c15
hujun <hujun@your-desired-host-name.local>
2016-03-31 21:48:28 +0800

    demo1

--- entry.js
+++ entry.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import TextDemo from "./components/divder"
+import TextDemo from "./components/demo1"
 
 render(
   <TextDemo>quesition</TextDemo>, 
   document.getElementById('mountNode')
 )
-
-
